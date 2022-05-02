94cc7fef487024ed79c5dd6ac5587a73676471ac
Olamilekan <abiolawalter2015@gmail.com>
2022-05-02 23:50:11 +0100

    performing DOM manipulations

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,23 @@
+// DOM MANIPULATION
+const numbers = document.querySelectorAll('data-number');
+const operators = document.querySelectorAll('data-operator');
+const equalsButton = document.querySelector("equals");
+const pointBtn = document.querySelector('point');
+const clearBtn = document.querySelector("clear__btn");
+const delBtn = document.querySelector('del__btn');
+const subScreen = document.querySelector("sub__screen");
+const mainScreen = document.querySelector("main__screen");
+
+// event functions
+window.addEventListener('keydown', handleClickEvent)
+
+
+//helper Functions
+//1
+function handleClickEvent(e) {
+    
+}
+
 // main calculator functions
 function add(a, b) {
     return a + b;
